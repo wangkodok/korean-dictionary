77694d4caa9de8b14edcf1ee609270a732069b13
wangkodok <wangkodok@gmail.com>
2024-07-05 23:43:22 +0900

    CORS 해결하기 위해 프록시 서버 설정 및 패키지 설치하고 데이터 출력
    axios, cors, express, http-proxy-middleware 패키지 설치 후 vite.config.js 파일에 프록시 서버 설정하고 API 호출 및 데이터 출력 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(
+        "/api/search.do?certkey_no=6692&key=0D517DC9B6391075F0D67AB963CAF077&type_search=search&req_type=json&q=나무"
+      );
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
   return <div>App</div>;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://stdict.korean.go.kr",
+    },
+  },
   plugins: [react()],
-})
+});
