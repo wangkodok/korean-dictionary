f5be8e0257ad02f6f0b794c2a40941592200d42b
wangkodok <wangkodok@gmail.com>
2024-07-06 19:11:10 +0900

    프록시 서버 재설정

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://stdict.korean.go.kr",
+      "/api": {
+        target: "https://stdict.korean.go.kr",
+        changeOrigin: true,
+      },
     },
   },
   plugins: [react()],
