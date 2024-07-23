13b7e2c46578c97b5e0ff1643ce6a1ff58117022
wangkodok <wangkodok@gmail.com>
2024-07-23 16:11:27 +0900

    Tailwind CSS, Redux 사용하여 반응형 웹 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { PiNotepadThin } from "react-icons/pi";
+import { useSelector, useDispatch } from "react-redux";
+
+export default function Header() {
+  const saveWord = useSelector((state) => {
+    return state.wordSave;
+  });
+
+  const dispatch = useDispatch();
+
+  function handleSideMenu() {
+    if (saveWord.length !== 0) {
+      dispatch({ type: "side-toggle", toggle: true });
+    }
+  }
+
+  return (
+    <header className="p-0 md:px-8">
+      <div className="p-8 flex justify-between items-center">
+        <a href="#" className="text-[20px] font-bold font-['Pretendard']">
+          ᄑᅀᅭᄌᆞᆫᄀᆞᆨᄋᆞᄃᆡᄉᆞᄌᆞᆫ
+        </a>
+        <button className="relative" onClick={handleSideMenu}>
+          <PiNotepadThin size={32} />
+          {saveWord.length === 0 ? null : (
+            <span className="absolute top-[-4px] right-[-4px] bg-red-500 text-[12px] text-white w-[20px] h-[20px] rounded-[10px] font-['Pretendard']">
+              {saveWord.length}
+            </span>
+          )}
+        </button>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,65 @@
+import { useSelector, useDispatch } from "react-redux";
+import { IoCloseOutline } from "react-icons/io5";
+
+export default function SideMenu() {
+  const saveWord = useSelector((state) => {
+    return state.wordSave;
+  });
+
+  const dispatch = useDispatch();
+
+  function handleDeleteWord(data) {
+    dispatch({ type: "word-delete", word: data });
+  }
+
+  function handleToggle() {
+    dispatch({ type: "side-toggle", toggle: false });
+  }
+
+  return (
+    <div className="fixed top-0 right-0 z-10 w-[17.5rem] bg-white shadow-lg shadow-slate-300/50 overflow-auto">
+      <div>
+        <div className="relative h-[100dvh] p-[2rem]">
+          <button
+            className="absolute top-[2rem] right-[2rem]"
+            onClick={handleToggle}
+          >
+            <IoCloseOutline size={32} />
+          </button>
+          {saveWord.length === 0 ? null : (
+            <div className="mt-12">
+              <div className="">
+                <p className="mb-6 text-[1.25rem] font-[900]">내 단어장</p>
+                <ul>
+                  {saveWord.map((data, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="pb-[1rem] mb-[1rem] border-b-[.0625rem]"
+                      >
+                        <dl>
+                          <dt className="mb-[.25rem]">
+                            <strong>{data.word}</strong>
+                          </dt>
+                          <dd className="mb-[2rem]">{data.sense.definition}</dd>
+                        </dl>
+                        <button
+                          className="block w-[100px] h-[48px] bg-slate-100 rounded-xl"
+                          onClick={() => {
+                            return handleDeleteWord(data);
+                          }}
+                        >
+                          단어 삭제
+                        </button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
