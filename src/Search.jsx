bb281547cf7874ec0469e1ea26f80d515d84ffdb
wangkodok <wangkodok@gmail.com>
2024-07-26 10:43:30 +0900

    axios 사용자 지정 에이전트

--- src/Search.jsx
+++ src/Search.jsx
@@ -61,6 +61,15 @@ export default function Search() {
             headers: {
               "Content-Type": "application/json",
               "Access-Control-Allow-Origin": "*",
+              "X-Requested-With": "XMLHttpRequest",
+              // "User-Agent":
+              //   "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
+              // "User-Agent": "xyz-bla-bla",
+              "user-agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+            },
+            proxy: {
+              protocol: "https",
             },
           }
         );
