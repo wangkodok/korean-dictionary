f006df6a61dabdada6a9c0e00724d89d5f07a96a
wangkodok <wangkodok@gmail.com>
2024-07-29 15:49:08 +0900

    fetch 요청에 headers 추가

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// import dns from "node:dns";
+
+// dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +11,11 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://stdict.korean.go.kr",
+        // target:
+        //   "https://stdict.korean.go.kr/api/search.do?certkey_no=6715&key=9685DE18F33A035667C656E856E9C401&type_search=search&req_type=json&q=%EB%82%98%EB%AC%B4",
         changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
