cfc27118ceed0aa0c3e597746bd2c81ed90548dc
wangkodok <wangkodok@gmail.com>
2024-08-12 23:36:31 +0900

    section 태그 내 제목 태그로 변경

--- src/components/MainVisual.jsx
+++ src/components/MainVisual.jsx
@@ -2,10 +2,10 @@ export default function MainVisual() {
   return (
     <section className="p-0 md:px-8">
       <div className="rounded-none flex justify-center items-center h-[12.5rem] bg-slate-100 md:rounded-xl ">
-        <div className="sm:flex text-[1.75rem] sm:text-[2rem] font-['Pretendard'] font-bold">
+        <h2 className="sm:flex text-[1.75rem] sm:text-[2rem] font-['Pretendard'] font-bold">
           <p className="text-center">표준국어대사전&#160;</p>
           <p>423,170 단어 검색</p>
-        </div>
+        </h2>
       </div>
     </section>
   );
