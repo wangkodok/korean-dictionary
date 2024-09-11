eccafa6bbcaf4091b7ba9ec249d22aa2f126ae59
wangkodok <wangkodok@gmail.com>
2024-09-11 09:41:04 +0900

    환경 변수 설정

--- src/pages/search/ui/Search.jsx
+++ src/pages/search/ui/Search.jsx
@@ -34,88 +34,21 @@ export default function Search() {
 
   const [queryData, setQueryData] = useState(""); // 사용자가 입력한 검색어를 관리하는 상태
 
-  // useEffect(() => {
-  //   if (query === "") return;
-  //   setLoading(true); // 스피너 start
-  //   const fetchData = (retryCount = 3) => {
-  //     fetch(
-  //       `/api/search.do?key=9685DE18F33A035667C656E856E9C401&type_search=search&req_type=json&q=${query}`,
-  //       {
-  //         method: "GET",
-  //         // headers: {
-  //         //   // "Content-Type": "text/json; charset=UTF-8",
-  //         //   // Accept: "text/json; charset=UTF-8",
-  //         //   // Authorization: `Bearer 9685DE18F33A035667C656E856E9C401`,
-  //         //   "Content-Language": "ko-KR",
-  //         //   // "Content-Type": "Application/json"
-  //         // },
-  //         headers: {
-  //           accept: "*/*",
-  //           "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-  //           "content-language": "ko-KR",
-  //           cookie:
-  //             "WMONID=MB6u9WrHETP; STCID=STC2&&NqCsmf4ZqYvvX47hX3kS1mm0HLyftXpHnRzQC0L5JpksTZGWpbQf!219788197!1725921497167; stdict=NqCsmf4ZqYvvX47hX3kS1mm0HLyftXpHnRzQC0L5JpksTZGWpbQf!219788197",
-  //           priority: "u=1, i",
-  //           referer: "https://korean-dictionary-three.vercel.app/",
-  //           "sec-ch-ua":
-  //             '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-  //           "sec-ch-ua-mobile": "?0",
-  //           "sec-ch-ua-platform": '"Windows"',
-  //           "sec-fetch-dest": "empty",
-  //           "sec-fetch-mode": "cors",
-  //           "sec-fetch-site": "same-origin",
-  //           "user-agent":
-  //             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
-  //         },
-  //       }
-  //     )
-  //       .then((response) => {
-  //         // 응답이 성공적이지 않으면 에러를 발생시킴
-  //         if (!response.ok) {
-  //           throw new Error("Network response was not ok");
-  //         }
-  //         // 응답 처리
-  //         return response.json(); // JSON 데이터로 파싱된 Promise를 반환
-  //       })
-  //       .then((data) => {
-  //         // 파싱된 데이터를 사용하여 처리
-  //         console.log(data);
-  //         setSearchResult(data);
-  //         setLoading(false);
-  //         setError(false);
-  //       })
-  //       .catch((error) => {
-  //         if (retryCount > 0) {
-  //           console.log(`Retrying... attempts left: ${retryCount}`);
-  //           setTimeout(() => fetchData(retryCount - 1), 1000); // 1초 후 재시도
-  //         } else {
-  //           setError(true);
-  //           setLoading(false);
-  //           console.error("Final error:", error);
-  //         }
-  //         // 요청 또는 응답에 오류가 발생한 경우 처리
-  //         console.error("Error:", error);
-  //       });
-  //   };
-  //   fetchData(); // 타이머 대신 즉시 API 호출
-  // }, [query]);
-
   useEffect(() => {
     if (query === "") return;
     setLoading(true); // 스피너 start
     const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
-
     const fetchData = (retryCount = 3) => {
       fetch(
-        // `https://stdict.korean.go.kr/api/search.do/key=9685DE18F33A035667C656E856E9C401&type_search=search&req_type=json&q=${query}`,
-        // `https://korean-dictionary-three.vercel.app/api/search.do?certkey_no=6715&key=9685DE18F33A035667C656E856E9C401&type_search=search&req_type=json&q=${query}`,
-        `${PROXY}/api/search.do?certkey_no=6715&key=9685DE18F33A035667C656E856E9C401&type_search=search&req_type=json&q=${query}`,
+        `${PROXY}/api/search.do?certkey_no=6715&key=${
+          import.meta.env.VITE_API_KEY
+        }&type_search=search&req_type=json&q=${query}`,
         {
           method: "GET",
           headers: {
             "Content-Type": "text/json; charset=UTF-8",
             Accept: "text/json; charset=UTF-8",
-            Authorization: `Bearer 9685DE18F33A035667C656E856E9C401`,
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
             "Content-Language": "ko-KR",
             // "Content-Type": "Application/json",
           },
