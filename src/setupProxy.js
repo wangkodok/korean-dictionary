619d7d3e8040814992bb585c7c7d814b89ce6a3e
wangkodok <wangkodok@gmail.com>
2024-09-13 18:26:42 +0900

    http-proxy-middleware 주석 처리

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,13 +8,13 @@
 //     })
 //   );
 // };
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: `https://stdict.korean.go.kr/api`,
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/api", {
+//       target: `https://stdict.korean.go.kr/api`,
+//       changeOrigin: true,
+//     })
+//   );
+// };
