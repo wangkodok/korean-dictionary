2fb5c0e09173c5b44923fbff8c7933d90841ebb2
wangkodok <wangkodok@gmail.com>
2024-09-13 11:07:36 +0900

    Open API url 요청 주소 변경하고 재배포 오류 확인

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,24 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   // Vite 개발 서버 설정 정의
-  server: {
-    // Proxy 설정
-    proxy: {
-      // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
-      "/api": {
-        // 요청 전달 대상 서버 주소 설정
-        target: "https://stdict.korean.go.kr/api",
-        // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
-        changeOrigin: true,
-        // 요청 경로에서 '/api' 제거
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        // SSL 인증서 검증 무시
-        secure: false,
-        // WebSocket 프로토콜 사용
-        ws: true,
-      },
-    },
-  },
+  // server: {
+  //   // Proxy 설정
+  //   proxy: {
+  //     // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
+  //     "/api": {
+  //       // 요청 전달 대상 서버 주소 설정
+  //       target: "https://stdict.korean.go.kr/api",
+  //       // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
+  //       changeOrigin: true,
+  //       // 요청 경로에서 '/api' 제거
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //       // SSL 인증서 검증 무시
+  //       secure: false,
+  //       // WebSocket 프로토콜 사용
+  //       ws: true,
+  //     },
+  //   },
+  // },
 
   resolve: {
     alias: {
